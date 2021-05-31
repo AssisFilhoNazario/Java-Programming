ce177ca15d5c544c1cf509882873875213fb84d0
Assis Filho <assisfilhopfic@gmail.com>
2021-05-31 10:09:01 -0300

    Objeto Carro

---
+++ cursoJava/orientadoObjeto/aula01Carro/Carro.java
@@ -0,0 +1,15 @@
+package aula01Carro;
+
+public class Carro {
+	String fabricante;
+	String modelo;
+	String cor;
+	int anoDeFabricacao;
+	Proprietario dono;
+	
+	void ligarAgora() {
+		System.out.println("Ligando o carro "+ modelo);
+	}
+	
+
+}
---
+++ cursoJava/orientadoObjeto/aula01Carro/Proprietario.java
@@ -0,0 +1,12 @@
+package aula01Carro;
+
+public class Proprietario {
+	
+	String nome;
+	String cpf;
+	int idade;
+	String logradouro;
+	String bairro;
+	String cidade;
+
+}
